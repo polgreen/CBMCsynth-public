259b1df9f4a60a7a63d9eb7b0d3ec7920da3b815
Julian Parsert <julian.parsert@uibk.ac.at>
2023-04-25 12:26:20 +0200

    fixed root_equality

--- src/CBMC_smt/util.cpp
+++ src/CBMC_smt/util.cpp
@@ -6,34 +6,60 @@
 #include <iostream>
 #include <format.h>
 #include <mathematical_expr.h>
+
 /*
  * Check if the root symbols in expr a and b are the same
  * */
 bool root_equality(const exprt& a, const exprt& b)
 {
-    if (a.id() == ID_function_application and b.id() == ID_function_application) {
-        const function_application_exprt a_app = to_function_application_expr(a);
-        const function_application_exprt b_app = to_function_application_expr(b);
-
-        if (a_app.id_string() == b_app.id_string()) {
-            return true;
+    if (a.id() != b.id()) {
+        return false;
+    } else if (a.is_constant()) {
+        if (!b.is_constant()) {
+            return false;
         }
-    } else if (a.id() == ID_equal and b.id() == ID_equal) {
-        return true;
-    } else if (a.is_constant() and b.is_constant()) {
         const constant_exprt a_cnst = to_constant_expr(a);
         const constant_exprt b_cnst = to_constant_expr(b);
 
-        if (a_cnst.get_value() == b_cnst.get_value()) {
+        if (a_cnst.get_value() != b_cnst.get_value()) {
+            return false;
+        } else {
             return true;
         }
-    } else if (a.id() == ID_symbol and b.id() == ID_symbol) {
+    } else if (b.is_constant()) {
+        if (! a.is_constant()) {
+            return false;
+        }
+        const constant_exprt a_cnst = to_constant_expr(a);
+        const constant_exprt b_cnst = to_constant_expr(b);
+        if (a_cnst.get_value() != b_cnst.get_value()) {
+            return false;
+        } else {
+            return true;
+        }
+    } else if (a.id() == ID_symbol) {
+        if (b.id() != ID_symbol) {
+            return false;
+        }
         const symbol_exprt a_expr = to_symbol_expr(a);
         const symbol_exprt b_expr = to_symbol_expr(b);
-
-        if (a_expr.get_identifier() == b_expr.get_identifier()) {
+        if (a_expr.get_identifier() != b_expr.get_identifier()) {
+            return false;
+        } else {
+            return true;
+        }
+    } else if (b.id() == ID_symbol) {
+        if (a.id() != ID_symbol) {
+            return false;
+        }
+        const symbol_exprt a_expr = to_symbol_expr(a);
+        const symbol_exprt b_expr = to_symbol_expr(b);
+        if (a_expr.get_identifier() != b_expr.get_identifier()) {
+            return false;
+        } else {
             return true;
         }
     }
-    return false;
+
+    return true;
 }
