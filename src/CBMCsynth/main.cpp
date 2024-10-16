8c855f67ba8d09cb0aa694243de8042aae7eb73e
Elizabeth Polgreen <epolgreen@gmail.com>
2024-10-16 10:37:50 +0100

    update help file

--- src/CBMCsynth/main.cpp
+++ src/CBMCsynth/main.cpp
@@ -28,7 +28,7 @@
   "(update-grammar)" \
   "(use-cex-in-prompt)" \
   "(do-not-expand-fun-apps)" \
-   "(program-depth): " \
+  "(program-depth): " \
   "(frequency-of-LLM-calls): " \
   "(astar)"                 \
   "(use-LLM)" \
@@ -64,27 +64,38 @@ void help(std::ostream &out)
          "Command line options\n"
          " --verbosity N                   increase verbosity (10 gives maximum verbosity)\n"
          "\n"
-         "The following options are applicable to SyGuS problems:\n"
-         "--cegis                          do CEGIS with the basic random top-down enumerator\n"
-         "--LLM-cegis                      do CEGIS with the basic random enumerator with LLM feedback\n"
-         "--use-bonus-weights              remove LLM grammar updates after 1 iteration\n"
-         "--update-grammar                 uses LLM feedback to update the grammar weights\n"
+         "The following options select the basic algorithm used for solving a CEGIS problem:\n"
+         "--cegis                          CEGIS with the most basic random top-down enumerator\n"
+         "                                 If no weights are given for the grammar, equal weights are used.\n"
+         "--astar                          CEGIS with the a* enumerator. If the --use-LLM option is\n" 
+         "                                 also specified, this CEGIS supports integrated calls to LLM\n"
+         "                                 If no weights are given for the grammar, equal weights are used.\n"
+         "--LLM-cegis                      CEGIS with the top-down random enumerator. If the --use-LLM option is\n" 
+         "                                 also specified, this CEGIS supports integrated calls to LLM\n"
+         
+         "\nThe following options are configurations for the LLM calls\n"
          "--use-cex-in-prompt              use the most recent solution and counterexample in the prompt\n"
-         "do-not-expand-fun-apps          do not expand function applications in the prompt\n"
-         "--cegis-bu                       do CEGIS with bottom up enumerator (no probabilities)\n"
-         "--cegis-prob-bu                  do CEGIS with bottom up enumerator with weights. \n"
-         "                                 we enumerate programs with the highest weight first\n"
+         "--do-not-expand-fun-apps         do not expand function applications in the prompt\n"
+         "--temperature                    set the temperature for openai calls\n"
+
+         "\nThe following options are configurations for the CEGIS algorithm\n"    
+         "--update-grammar                 uses LLM feedback to update the grammar weights\n"
          "--add-default-grammar            replace all synthesis function grammars with default\n"
-         "--dump-problem                   print problem in SyGuS-IF format\n"
-         "--dump-problem-as-smt            use quantifiers to print a sygus problem in SMT-LIB\n"
-         "--solve-with-cvc5                call CVC5 to solve the SyGuS problem\n"
-         "--probs F                        give a file with weights for each rule. The weights must be integers,\n"
+         "--program-depth                  limit the maximum depth we enumerate in top-down CEGIS (default 5) \n"
+         "--use-LLM                        make calls to the LLM during synthesis\n"
+         "--probs F                        give a file with weights for each production rule. The weights must be integers,\n"
          "                                 one integer per line, in the order that the NTs and rules appear in the grammar\n"
          "\n"
          "\n"
-         "The following options are applicable to SMT problems:\n"
-         "--solve-smt                      solve the SMT problem with Z3\n"
-         "--dump-problem                   print problem in SMT-lib format\n"
+
+        "The following options are useful printing and debugging options\n"
+         "--dump-problem                   print problem in SyGuS-IF format\n"
+         "--dump-problem-as-smt            use quantifiers to print a sygus problem in SMT-LIB\n"
+         "--solve-with-cvc5                call CVC5 to solve the SyGuS problem\n"
+         
+        //  "The following options are debugging optoin to SMT problems:\n"
+        //  "--solve-smt                      solve the SMT problem with Z3\n"
+        //  "--dump-problem                   print the SMT problem in SMT-lib format\n"
          "\n"
          "\n";
 }
